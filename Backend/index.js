171817df168859f32ffdde0d6ba6e0afcda2a597
iamujjwal08 <ujjwalkumar91622408@gmil.com>
2026-07-30 19:52:50 +0530

    Fix path import and frontend path

--- Backend/index.js
+++ Backend/index.js
@@ -38,7 +38,7 @@ app.use("/api/analytics", analyticsRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../Frontend/dist")));
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.resolve(__dirname, "../Frontend/dist/index.html"));
 });
 } else {
